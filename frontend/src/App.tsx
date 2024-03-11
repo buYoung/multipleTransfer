181a6194e52a0b1ec7ac69d58d6f396c79a99218
buyong <leebu18@gmail.com>
2024-03-11 21:32:30 +0900

    feat(main ui):
    main ui 설정중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,11 @@ import {useState, useRef} from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card.tsx";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table.tsx";
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/components/ui/context-menu.tsx";
-import * as ContextMenuPrimitive from "@radix-ui/react-context-menu";
+
 import { Button } from "@/components/ui/button.tsx";
+import { Separator } from "@/components/ui/separator.tsx";
+import { Checkbox } from "@/components/ui/checkbox.tsx";
+import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 // import {Greet} from "wailsjs/go/main/App";
 
 interface Host {
@@ -11,7 +14,6 @@ interface Host {
     host: string;
     port: string;
     sshKey: string;
-    path: string;
 }
 
 function App() {
@@ -25,8 +27,25 @@ function App() {
     }
 
     const data: Host[] = [
-        { userId: "test", host: "192.168.0.1", port: "22", sshKey: "test", path: "/home/test" },
-        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test", path: "/home/test" },
+        { userId: "test", host: "192.168.0.1", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
+        { userId: "test", host: "192.168.0.2", port: "22", sshKey: "test" },
     ]
 
     return (
@@ -39,54 +58,151 @@ function App() {
                         <CardDescription>서버로 전송할 선택된 파일 목록을 표시합니다.</CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <ContextMenu>
-                            <ContextMenuTrigger>
-                                <Table>
-                                    <TableHeader>
-                                        <TableRow>
-                                            <TableHead className="w-[20%] text-center">UserId</TableHead>
-                                            <TableHead className="w-[30%] text-center">Host</TableHead>
-                                            <TableHead className="w-[10%] text-center">Port</TableHead>
-                                            <TableHead className="w-[20%] text-center">sshKey</TableHead>
-                                            <TableHead className="w-[20%] text-center">Action</TableHead>
-                                        </TableRow>
-                                    </TableHeader>
-                                    <TableBody>
-                                        {
-                                            data.map((host, index) => {
-                                                return (
-                                                    <TableRow key={index}>
-                                                        <TableCell className="font-medium text-center">{host.userId}</TableCell>
-                                                        <TableCell className="text-center">{host.host}</TableCell>
-                                                        <TableCell className="text-center">{host.port}</TableCell>
-                                                        <TableCell className="text-center">{host.sshKey}</TableCell>
-                                                        <TableCell className="flex justify-evenly">
-                                                            <Button>수정</Button>
-                                                            <Button className="bg-red-600 hover:bg-red-500">삭제</Button>
-                                                        </TableCell>
-                                                    </TableRow>
-                                                )
-                                            })
-                                        }
-                                    </TableBody>
-                                </Table>
-                            </ContextMenuTrigger>
-                            <ContextMenuContent
-                                sticky={'always'}
-                                onCloseAutoFocus={(e) => {
-                                    console.log("onCloseAutoFocus", e)
-                                }}
-                            >
-                                <ContextMenuItem>Profile</ContextMenuItem>
-                                <ContextMenuItem>Billing</ContextMenuItem>
-                                <ContextMenuItem>Team</ContextMenuItem>
-                                <ContextMenuItem>Subscription</ContextMenuItem>
-                            </ContextMenuContent>
-                        </ContextMenu>
+                        <div className="w-full">
+                            <div className="flex justify-between pb-3">
+                                <div className="flex items-center">
+                                    <span className="pr-5 text-center">
+                                        파일 개수 : { data.length }
+                                    </span>
+                                    <span className="pr-5 text-center">
+                                        선택된 파일 개수 : { data.length }
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <div className="pr-3 flex items-center">
+                                    <Checkbox id="sendfor-compress"/>
+                                        <label htmlFor="sendfor-compress" className="pl-2 select-none">서버 전송시 파일 압축</label>
+                                    </div>
+                                    <Button>추가</Button>
+                                </div>
+                            </div>
+                            <ContextMenu>
+                                <ContextMenuTrigger>
+                                    <ScrollArea className="h-72 w-full">
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead className="w-[5%] text-center sticky top-0">
+                                                        <Checkbox/>
+                                                    </TableHead>
+                                                    <TableHead className="w-[20%] text-center">UserId</TableHead>
+                                                    <TableHead className="w-[30%] text-center">Host</TableHead>
+                                                    <TableHead className="w-[10%] text-center">Port</TableHead>
+                                                    <TableHead className="w-[20%] text-center">sshKey</TableHead>
+                                                    <TableHead className="w-[15%] text-center">Action</TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            <TableBody>
+                                                {
+                                                    data.map((host, index) => {
+                                                        return (
+                                                            <TableRow key={index}>
+                                                                <TableCell className="text-center">
+                                                                    <Checkbox/>
+                                                                </TableCell>
+                                                                <TableCell className="font-medium text-center">{host.userId}</TableCell>
+                                                                <TableCell className="text-center">{host.host}</TableCell>
+                                                                <TableCell className="text-center">{host.port}</TableCell>
+                                                                <TableCell className="text-center">{host.sshKey}</TableCell>
+                                                                <TableCell className="flex justify-center items-center space-x-4 text-sm">
+                                                                    <Button>수정</Button>
+                                                                    <Separator orientation="vertical" className="h-6"/>
+                                                                    <Button className="bg-red-600 hover:bg-red-500">삭제</Button>
+                                                                </TableCell>
+                                                            </TableRow>
+                                                        )
+                                                    })
+                                                }
+                                            </TableBody>
+                                        </Table>
+                                    </ScrollArea>
+                                </ContextMenuTrigger>
+                                <ContextMenuContent
+                                    sticky={'always'}
+                                    onCloseAutoFocus={(e) => {
+                                        console.log("onCloseAutoFocus", e)
+                                    }}
+                                >
+                                    <ContextMenuItem>Profile</ContextMenuItem>
+                                    <ContextMenuItem>Billing</ContextMenuItem>
+                                    <ContextMenuItem>Team</ContextMenuItem>
+                                    <ContextMenuItem>Subscription</ContextMenuItem>
+                                </ContextMenuContent>
+                            </ContextMenu>
+                        </div>
                     </CardContent>
-                    <CardFooter>
-                        <p>Card Footer</p>
-                    </CardFooter>
+                </Card>
+                <Separator className="mt-3 mb-3 bg-transparent" />
+                <Card>
+                    <CardHeader>
+                        <CardTitle>서버목록</CardTitle>
+                        <CardDescription>사용할 서버 목록을 표시합니다.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="w-full">
+                            <div className="flex justify-between pb-3">
+                                <div className="flex items-center">
+                                    <span className="pr-5 text-center">
+                                        서버 개수 : { data.length }
+                                    </span>
+                                </div>
+                                <Button>추가</Button>
+                            </div>
+                            <ContextMenu>
+                                <ContextMenuTrigger>
+                                    <ScrollArea className="h-72 w-full">
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead className="w-[5%] text-center">
+                                                        <Checkbox/>
+                                                    </TableHead>
+                                                    <TableHead className="w-[20%] text-center">UserId</TableHead>
+                                                    <TableHead className="w-[30%] text-center">Host</TableHead>
+                                                    <TableHead className="w-[10%] text-center">Port</TableHead>
+                                                    <TableHead className="w-[20%] text-center">sshKey</TableHead>
+                                                    <TableHead className="w-[15%] text-center">Action</TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            <TableBody>
+                                                {
+                                                    data.map((host, index) => {
+                                                        return (
+                                                            <TableRow key={index}>
+                                                                <TableCell className="text-center">
+                                                                    <Checkbox/>
+                                                                </TableCell>
+                                                                <TableCell className="font-medium text-center">{host.userId}</TableCell>
+                                                                <TableCell className="text-center">{host.host}</TableCell>
+                                                                <TableCell className="text-center">{host.port}</TableCell>
+                                                                <TableCell className="text-center">{host.sshKey}</TableCell>
+                                                                <TableCell className="flex justify-center items-center space-x-4 text-sm">
+                                                                    <Button>수정</Button>
+                                                                    <Separator orientation="vertical" className="h-6"/>
+                                                                    <Button className="bg-red-600 hover:bg-red-500">삭제</Button>
+                                                                </TableCell>
+                                                            </TableRow>
+                                                        )
+                                                    })
+                                                }
+                                            </TableBody>
+                                        </Table>
+                                    </ScrollArea>
+                                </ContextMenuTrigger>
+                                <ContextMenuContent
+                                    sticky={'always'}
+                                    onCloseAutoFocus={(e) => {
+                                        console.log("onCloseAutoFocus", e)
+                                    }}
+                                >
+                                    <ContextMenuItem>Profile</ContextMenuItem>
+                                    <ContextMenuItem>Billing</ContextMenuItem>
+                                    <ContextMenuItem>Team</ContextMenuItem>
+                                    <ContextMenuItem>Subscription</ContextMenuItem>
+                                </ContextMenuContent>
+                            </ContextMenu>
+                        </div>
+                    </CardContent>
                 </Card>
             </div>
             {/*<div id="result" className="result">{resultText}</div>*/}
